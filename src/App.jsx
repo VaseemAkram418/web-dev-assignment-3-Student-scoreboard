c32bd144b7515ec48b5b5b6fe34e4ac2b98dc863
Vaseem Akram <vaseemakrama366@gmail.com>
2026-04-30 20:47:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Header from "./components/header";
+import StudentTable from "./components/StudentTable";
+import AddStudentForm from "./components/AddStudentForm";
+import "./App.css";
+
+function App() {
+  const [students, setStudents] = useState([
+    { id: 1, name: "Rahul", score: 50 },
+    { id: 2, name: "Aman", score: 30 },
+  ]);
+
+  const addStudent = (name, score) => {
+    const newStudent = {
+      id: Date.now(),
+      name,
+      score: Number(score),
+    };
+    setStudents([...students, newStudent]);
+  };
+
+  const updateScore = (id, newScore) => {
+    const updated = students.map((s) =>
+      s.id === id ? { ...s, score: Number(newScore) } : s
+    );
+    setStudents(updated);
+  };
+
+  return (
+    <div>
+      <Header />
+      <AddStudentForm addStudent={addStudent} />
+      <StudentTable students={students} updateScore={updateScore} />
+    </div>
+  );
+}
+
+export default App;
